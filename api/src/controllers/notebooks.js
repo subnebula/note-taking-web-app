4febf6ed742b93829078e7c4442041cc2dbf1ddf
Aiden Nibali <dismaldenizen@gmail.com>
2018-02-24 10:57:51 +1100

    Provide the notebook index endpoint

--- api/src/controllers/notebooks.js
+++ api/src/controllers/notebooks.js
@@ -4,6 +4,13 @@ const models = require('../models');
 
 const router = express.Router();
 
+// Index
+router.get('/', (req, res) => {
+  models.Notebook.findAll({ order: [['createdAt', 'DESC']] })
+    .then(notebooks => res.json(notebooks))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
 /* *** TODO: Fill in the API endpoints for notebooks *** */
 
 module.exports = router;
