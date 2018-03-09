df4dcf725821053cc3fe9988ebc056a2cea94223
Aiden Nibali <dismaldenizen@gmail.com>
2018-03-10 10:13:54 +1100

    Add Youch error handling to frontend

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -11,6 +11,7 @@
 
 const path = require('path');
 const express = require('express');
+const Youch = require('youch');
 
 // Create a new Express app
 const app = express();
@@ -24,31 +25,36 @@ app.use('/assets/font-awesome/fonts', express.static(
 
 // Set up the index route
 app.get('/', (req, res) => {
-  try {
-    const htmlDocument = `<!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="utf-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
+  const htmlDocument = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-          <title>Neverwrote</title>
-          <link rel="stylesheet" type="text/css" href="/assets/css/app.css">
-          <script src="/assets/js/vendor.js"></script>
-          <script src="/assets/js/app.js"></script>
-        </head>
-        <body>
-          <div id="root"></div>
-          <script>main();</script>
-        </body>
-      </html>`;
+        <title>Neverwrote</title>
+        <link rel="stylesheet" type="text/css" href="/assets/css/app.css">
+        <script src="/assets/js/vendor.js"></script>
+        <script src="/assets/js/app.js"></script>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>main();</script>
+      </body>
+    </html>`;
 
-    // Respond with the complete HTML page
-    res.send(htmlDocument);
-  } catch(ex) {
-    console.error(ex.stack);
-    res.status(500).send(ex.stack);
+  // Respond with the complete HTML page
+  res.send(htmlDocument);
+});
+
+// Catch-all for handling errors.
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  if(res.headersSent) {
+    return next(err);
   }
+  const youch = new Youch(err, req);
+  youch.toHTML().then(html => res.send(html));
 });
 
 module.exports = app;
