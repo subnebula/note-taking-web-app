9229faf04110e5c3b8265d8783e7319b09e408cc
Michael Pettit <18519961@students.latrobe.edu.au>
2018-10-09 22:47:56 +1100

    almost done notebooks

--- api/src/controllers/notebooks.js
+++ api/src/controllers/notebooks.js
@@ -13,4 +13,45 @@ router.get('/', (req, res) => {
 
 /* *** TODO: Fill in the API endpoints for notebooks *** */
 
+// Get /notebooks/:notebookID/notes
+/*router.get('/:notebookId/notes', (req, res) => {
+  models.Notebook.findById(req.params.notebookId)
+    .then(models.Notes.findAll({ order: [['createdAt', 'DESC']] })update)
+    .then(notebooks => res.json(notebooks))
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json({ error: err.message }));
+});*/
+
+// Post /notebooks
+router.post('/', (req, res) => {
+  models.Notebook.create(req.body)
+    .then(notebooks => res.json(notebooks))
+    .catch(err => res.status(422).json({ error: err.message }));
+});
+
+// Get /notebooks/:notebookId
+router.get('/:notebookId', (req, res) => {
+  models.Notebook.findById(req.params.notebookId)
+    .then(notebooks => res.json(notebooks))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// Delete /notebooks/:notebookId
+router.delete('/:notebookId', (req, res) => {
+  models.Notebook.destroy({ where: { id: req.params.notebookId } })
+    .then(() => res.json({}))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+
+// Put /notebooks/:notebookID
+router.put('/:notebookId', (req, res) => {
+  models.Notebook.update(
+    { title: req.body },
+    { where: { id: req.params.notebookId } })
+    .then(notebooks => res.json(notebooks))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+
 module.exports = router;
