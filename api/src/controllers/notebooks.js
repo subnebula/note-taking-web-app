ddeb58fe20ce77b186bef5a264125cefa2139553
Michael Pettit <18519961@students.latrobe.edu.au>
2018-10-10 16:25:02 +1100

    Finsished task 1

--- api/src/controllers/notebooks.js
+++ api/src/controllers/notebooks.js
@@ -13,4 +13,44 @@ router.get('/', (req, res) => {
 
 /* *** TODO: Fill in the API endpoints for notebooks *** */
 
+// GET /notebooks/:notebookID/notes
+router.get('/:notebookId/notes', (req, res) => {
+  models.Note.findAll({ where: {notebookId: req.params.notebookId } })
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// POST /notebooks
+router.post('/', (req, res) => {
+  models.Notebook.create(req.body)
+    .then(notebook => res.json(notebook))
+    .catch(err => res.status(422).json({ error: err.message }));
+});
+
+// GET /notebooks/:notebookId
+router.get('/:notebookId', (req, res) => {
+  models.Notebook.findById(req.params.notebookId)
+    .then(notebook => res.json(notebook))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// DELETE /notebooks/:notebookId
+router.delete('/:notebookId', (req, res) => {
+  models.Notebook.destroy({ where: { id: req.params.notebookId } })
+    .then(() => res.json({}))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+
+// PUT /notebooks/:notebookID
+router.put('/:notebookId', (req, res) => {
+  models.Notebook.update(
+    { title: req.body.title },
+    { where: { id: req.params.notebookId } })
+    .catch(err => res.status(500).json({ error: err.message }));
+  models.Notebook.findById(req.params.notebookId)
+    .then(notebook => res.json(notebook))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
 module.exports = router;
--- api/src/controllers/notes.js
+++ api/src/controllers/notes.js
@@ -6,4 +6,45 @@ const router = express.Router();
 
 /* *** TODO: Fill in the API endpoints for notes *** */
 
+// GET /notes
+router.get('/', (req, res) => {
+  models.Note.findAll({ order: [['createdAt', 'DESC']] })
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// POST /notes
+router.post('/', (req, res) => {
+  models.Note.create(req.body)
+    .then(note => res.json(note))
+    .catch(err => res.status(422).json({ error: err.message }));
+});
+
+// GET /notes/:noteId
+router.get('/:noteId', (req, res) => {
+  models.Note.findById(req.params.noteId)
+    .then(note => res.json(note))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// DELETE /notes/:noteId
+router.delete('/:noteId', (req, res) => {
+  models.Note.destroy({ where: { id: req.params.noteId } })
+    .then(() => res.json({}))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// PUT /notes/:noteId
+router.put('/:noteId', (req, res) => {
+  models.Note.update(
+    { title: req.body.title,
+      content: req.body.content,
+      notebookId: req.body.notebookId },
+    { where: { id: req.params.noteId } })
+    .catch(err => res.status(500).json({ error: err.message }));
+  models.Note.findById(req.params.noteId)
+    .then(note => res.json(note))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
 module.exports = router;
